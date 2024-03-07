9ba8ab1159f81ec93828eb7ea9932554363e1709
Felipe9929 <luis.velasquez1@catolica.edu.sv>
2024-03-07 14:55:18 -0600

    Agregar archivos de proyecto.

---
+++ PracticawebAPI2/Models/equipos2Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace PracticawebAPI2.Models
+{
+    public class equipos2Context: DbContext
+    {
+        public equipos2Context(DbContextOptions<equipos2Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<equipos2> equipos2 { get; set; }
+
+    }
+}
